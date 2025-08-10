65188dff659f08786522227157137e2c8b725b53
red <lurobaca@users.noreply.github.com>
2025-08-10 14:56:09 -0600

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// IMPORTANT: Cambia 'Essco' si tu repo tiene otro nombre
-export default defineConfig({
-  plugins: [react()],
-  base: '/Essco/'
-})
